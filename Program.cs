d6e85f692f0e009678a19f407439961fcaf4f396
lia <gukamelia707@gmail.com>
2024-12-10 14:40:30 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp567
+{
+    class Program
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            //string[] people = { "Tom", "Sam", "Bob", "Kate", "Tom", "Alice" };
+            //Array.Sort(people);
+            //foreach (var el in people)
+            //{
+            //    Console.WriteLine(el);
+            //}
+            //Console.WriteLine();
+            //Array.Reverse(people);
+            //foreach (var el in people)
+            //{
+            //    Console.WriteLine(el);
+            //}
+            //Console.ReadLine();
+        }
+    }
+}
